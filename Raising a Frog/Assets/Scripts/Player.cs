3d147ac3b1a29f47ffb52847a925b74ee8bb8056
정수빈 <banyeong321@gmail.com>
2021-05-19 22:01:42 +0900

    위 아래 움직임, 좌우 벽(노란색) 통과 막기 막 부화한 올챙이만 구현함. 움직일 때 헤엄치는 애니메이션 아직 미적용.

--- Raising a Frog/Assets/Scripts/Player.cs
+++ Raising a Frog/Assets/Scripts/Player.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
+    public BoxCollider2D playerCollider;
+
+    [SerializeField] //인스펙터 창에 보이기 하기 위함
     private int click; //개구리알_스페이스바 연타시 증가
+
+    public Rigidbody2D playerRigidbody; //플레이어 리지드바디
+    public float speed = 8f;
 
     void Start()
     {
+        playerCollider = GetComponent<BoxCollider2D>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
         click = 0;
     }
 
@@ -23,17 +30,34 @@ public class Player : MonoBehaviour
             }
             if (click >= 100)
             {
-                GameObject.Find("GameManager").GetComponent<GameManager>().step = 1;
+                //step을 1로 만듦
+                GameObject.Find("GameManager").GetComponent<GameManager>().step++;
                 Evolution();
             }
         }
+        else
+        {
+            float xInput = Input.GetAxis("Horizontal");
+            float yInput = Input.GetAxis("Vertical");
+
+            float xSpeed = xInput * speed;
+            float ySpeed = yInput * speed;
+
+            Vector2 newVelocity = new Vector2(xSpeed, ySpeed);
+            //Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+
+            playerRigidbody.velocity = newVelocity;
+        }
     }
 
-    public void Evolution() //개구리 진화
+    // *     개구리 진화     *
+    public void Evolution()
     {
+        //막 부화한 올챙이라면
         if (GameObject.Find("GameManager").GetComponent<GameManager>().step == 1)
         {
-            GetComponent<Animator>().SetInteger("step", 1);
+            GetComponent<Animator>().SetInteger("step", 1); //애니메이터 변수 step 1로 바꿈
+            playerCollider.size = new Vector3(2.7f, 1.516f, 1f); //콜라이더 사이즈 변경
         }
     }
 }
