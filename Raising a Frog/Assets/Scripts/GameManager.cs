28d40851ec41f1b26a83d0f48ae0914372d57f30
정수빈 <banyeong321@gmail.com>
2021-05-19 19:42:44 +0900

    Step이 0일 때(개구리알), 스페이스바를 누르면 int click 변수의 증가

--- Raising a Frog/Assets/Scripts/GameManager.cs
+++ Raising a Frog/Assets/Scripts/GameManager.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // *      캐릭터 스탯       *
-    private int charm; //매력
-    private int intell; //지능
-    private int wealth; //재력
-    private int inqMind; //탐구심
-    
-    // *      캐릭터 성장       *
-    private int step; //성장 단계
+    public int charm; //매력
+    public int intell; //지능
+    public int wealth; //재력
+    public int inqMind; //탐구심
+
+    public int step; //성장 단계
+    // 0: 개구리알 / 1: 올챙이 / 2: 올챙이 뒷다리 / 3: 올챙이 앞다리
+    // 4: 꼬리짧아짐
 
     void Start()
     {
@@ -20,10 +21,11 @@ public class GameManager : MonoBehaviour
         intell = 0;
         wealth = 0;
         inqMind = 0;
+        step = 0;
     }
 
     void Update()
-    {
-        
+    {  
+
     }
 }
--- Raising a Frog/Assets/Scripts/Player.cs
+++ Raising a Frog/Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private int click; //개구리알 클릭
+    public Camera MainCamera;
 
-    
     void Start()
     {
         
+
+        click = 0;
     }
 
     void Update()
     {
-        
+        // 개구리 알일 때
+        if (GameObject.Find("GameManager").GetComponent<GameManager>().step == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                click++;
+            }
+        }
     }
 }
