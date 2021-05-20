07a41d2fda1e1d2923d8d336cf0d3514ff034a40
정수빈 <banyeong321@gmail.com>
2021-05-20 19:12:26 +0900

    앞다리 나온 개구리까지 애니메이터 상에서 구현 완료

--- Raising a Frog/Assets/Scripts/Player.cs
+++ Raising a Frog/Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     public BoxCollider2D playerCollider;
 
     [SerializeField] //인스펙터 창에 보이기 하기 위함
-    private int click; //개구리알_스페이스바 연타시 증가
+    private int spaceBar; //개구리알_스페이스바 연타시 증가
 
     public Rigidbody2D playerRigidbody; //플레이어 리지드바디
     public float speed = 8f;
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
     {
         playerCollider = GetComponent<BoxCollider2D>();
         playerRigidbody = GetComponent<Rigidbody2D>();
-        click = 0;
+        spaceBar = 0;
     }
 
     void Update()
@@ -26,9 +26,9 @@ public class Player : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space)) //스페이스바
             {
-                click++;
+                spaceBar++;
             }
-            if (click >= 50)
+            if (spaceBar >= 50)
             {
                 //step을 1로 만듦
                 GameObject.Find("GameManager").GetComponent<GameManager>().step++;
@@ -58,8 +58,8 @@ public class Player : MonoBehaviour
                 transform.localScale = new Vector2(-1, 1);
             }
 
-            // *      움직이는 중인지      *
-            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            // *      헤엄치는 중인가?      *
+            if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
             {
                 GetComponent<Animator>().SetBool("isSwimming", true);
             }
@@ -73,17 +73,25 @@ public class Player : MonoBehaviour
     // *     개구리 진화     *
     public void Evolution()
     {
-        //막 부화한 올챙이라면
+        //2, 막 부화한 올챙이로 변경
         if (GameObject.Find("GameManager").GetComponent<GameManager>().step == 1)
         {
             GetComponent<Animator>().SetInteger("step", 1); //애니메이터 변수 step 1로 바꿈
             playerCollider.size = new Vector3(2.7f, 1.516f, 1f); //콜라이더 사이즈 변경
         }
-        //2단계 뒷다리가 나온 올챙이라면
+        //3, 뒷다리가 나온 올챙이로 변경
         if (GameObject.Find("GameManager").GetComponent<GameManager>().step == 2)
         {
-            GetComponent<Animator>().SetInteger("step", 2); //애니메이터 변수 step 1로 바꿈
+            GetComponent<Animator>().SetInteger("step", 2); //애니메이터 변수 step 2로 바꿈
+            speed = 10f;
             //playerCollider.size = new Vector3(2.7f, 1.516f, 1f); //콜라이더 사이즈 변경
+        }
+        //4, 앞다리가 나온 올챙이로 변경
+        if (GameObject.Find("GameManager").GetComponent<GameManager>().step == 3)
+        {
+            GetComponent<Animator>().SetInteger("step", 3); //애니메이터 변수 step 3로 바꿈
+            speed = 11f;
+            playerCollider.size = new Vector3(3.723f, 1.646f, 1f); //콜라이더 사이즈 변경
         }
     }
 }
